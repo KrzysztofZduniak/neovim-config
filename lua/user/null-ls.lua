70b05a72696dfe3d6a1d2a8e01114e39a7bb5894
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2022-02-02 14:45:35 +0100

    basics

---
+++ lua/user/null-ls.lua
@@ -0,0 +1,22 @@
+local status_ok, null_ls = pcall(require, "null-ls")
+if not status_ok then
+	return
+end
+
+local formatting = null_ls.builtins.formatting
+
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+	debug = false,
+	sources = {
+		-- Lua
+		formatting.stylua,
+		-- python
+		formatting.black.with({ extra_args = { "--fast" } }),
+		diagnostics.flake8,
+
+    -- rust
+    formatting.rustfmt
+	},
+})
