7eefa4fddbbb4469b246320ad83d92e7735d6007
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2022-04-30 15:06:13 +0200

    clean up server setup

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -3,6 +3,13 @@ if not status_ok then
 	return
 end
 
+local servers = {
+	"sumneko_lua",
+	"pyright",
+	"tsserver",
+	"rust_analyzer",
+}
+
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
@@ -11,16 +18,31 @@ lsp_installer.on_server_ready(function(server)
 		capabilities = require("user.lsp.handlers").capabilities,
 	}
 
-	if server.name == "sumneko_lua" then
-		local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	for _, s in ipairs(servers) do
+		if server.name == s then
+			local server_opts = require("user.lsp.settings."..s)
+			opts = vim.tbl_deep_extend("force", server_opts, opts)
+		end
 	end
 
-	if server.name == "pyright" then
-		local pyright_opts = require("user.lsp.settings.pyright")
-		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	end
-
+	-- if server.name == "sumneko_lua" then
+	-- 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+	-- 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	-- end
+	--
+	-- if server.name == "pyright" then
+	-- 	local pyright_opts = require("user.lsp.settings.pyright")
+	-- 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	-- end
+	-- if server.name == "tsserver" then
+	-- 	local pyright_opts = require("user.lsp.settings.tsserver")
+	-- 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	-- end
+	-- if server.name == "rust_analyzer" then
+	-- 	local pyright_opts = require("user.lsp.settings.rust_analyzer")
+	-- 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	--  end
+	--
 	-- if server.name == "hls" then
 	-- 	local hls = require("user.lsp.settings.hls")
 	-- 	opts = vim.tbl_deep_extend("force", hls, opts)
@@ -28,10 +50,6 @@ lsp_installer.on_server_ready(function(server)
 	-- if server.name == "clangd" then
 	--   local clangd_opts = require("user.lsp.settings.clangd")
 	--   opts = vim.tbl_deep_extend("force", clangd_opts, opts)
-	-- end
-
-	-- if server.name == "rust_analyzer" then
-	--   local rust_analyzer_opts = require
 	-- end
 
 	-- This setup() function is exactly the same as lspconfig's setup function.
