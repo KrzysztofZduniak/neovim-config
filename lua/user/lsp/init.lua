0b9b4001560e4263e663d98b9e2fb105e0156756
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2022-05-14 11:02:33 +0200

    Update completion plugin

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -20,13 +20,13 @@ local servers = {
 for _, server in ipairs(servers) do
   local status_ok, settings = pcall(require, 'user.lsp.settings.'..server)
   if not status_ok then
-    vim.api.nvim_echo( {{'settings not found for server' .. server, '' }})
+    vim.api.nvim_echo( {{'settings not found for server' .. server }}, false, {})
     settings = {}
   end
   lspconfig[server].setup{
     on_attach = handlers.on_attach,
     settings = settings,
-    -- capabilities = handlers.capabilities,
+    capabilities = handlers.capabilities,
   }
 end
 
