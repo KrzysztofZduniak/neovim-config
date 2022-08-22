1774a2033426b623e750497bcd81f4890f8a4030
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2022-08-22 20:01:02 +0200

    Temporarily disable bufferline

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -128,7 +128,7 @@ return packer.startup(function(use)
 	use("kyazdani42/nvim-tree.lua")
 
 	-- --------- bufferline -------------
-	use("akinsho/bufferline.nvim")
+	--[[ use("akinsho/bufferline.nvim") ]]
 	use("moll/vim-bbye")
 
 	-- --------- Latex ------------------
